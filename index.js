7335020b6f0833619e7587601e775042265861f8
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-24 11:55:50 -0500

    Add event listeners to  index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import UI from './modules/UI.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#addButton').onclick = UI.addNewBook;
+  document.querySelector('.menuList').onclick = UI.showSection;
+  UI.displayBooks();
+});
