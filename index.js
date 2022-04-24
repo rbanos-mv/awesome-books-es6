9be0cabf99515c05701f465a59cf794026580462
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-24 12:34:11 -0500

    Call displayDate

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#addButton').onclick = UI.addNewBook;
   document.querySelector('.menuList').onclick = UI.showSection;
   UI.displayBooks();
+  UI.displayDate();
 });
